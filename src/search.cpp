69e21002fd0d44d5f9e00a0ae953f69ecafd9039
b1f6c1c4 <b1f6c1c4@gmail.com>
2025-01-26 17:27:54 -0500

    screening

--- src/search.cpp
+++ src/search.cpp
@@ -64,16 +64,19 @@ constexpr inline board_t<11> operator ""_b11(const char *str, size_t len) {
 }
 
 template <size_t L>
+bool screen(const std::vector<Piece<L>> &lib, board_t<L> board) {
+    using namespace std::placeholders;
+    auto func = std::bind(solve_count<L>, lib, _1);
+    auto b = board.base;
+    for (auto r : board.regions)
+        b = b.clear(r.front());
+    return !solve(lib, b, true).empty();
+}
+
+template <size_t L>
 stat_t evaluate(const std::vector<Piece<L>> &lib, board_t<L> board, bool abort_on_zero) {
     using namespace std::placeholders;
     auto func = std::bind(solve_count<L>, lib, _1);
-    if (abort_on_zero) {
-        auto b = board.base;
-        for (auto r : board.regions)
-            b = b.clear(r.front());
-        if (!func(b))
-            return { 1 };
-    }
     boost::basic_thread_pool pool;
     std::vector<boost::future<size_t>> futures;
     [&](this auto &&self, auto it, Shape<L> curr) {
@@ -104,34 +107,39 @@ stat_t evaluate(const std::vector<Piece<L>> &lib, board_t<L> board, bool abort_o
     return stat;
 }
 
-int enumerate_library(size_t area, char *argv[]) {
-    static constexpr size_t L = 8;
-
+int enumerate_library(size_t area, auto &&screening, char *argv[]) {
     auto min_piece_size = std::stoul(argv[1]);
     auto max_piece_size = std::stoul(argv[2]);
     auto min_pieces = std::stoul(argv[3]);
     auto max_pieces = std::stoul(argv[4]);
 
-    std::vector<Shape<L>> shapes;
-    size_t possible_shapes{};
+    std::vector<Shape<8>> shapes;
+    size_t all_shapes{}, possible_shapes{};
     auto valid = ::fcntl(3, F_GETFD) != -1 || errno != EBADF;
     if (valid) {
         std::print("writing to fd:3 ({})\n",
             std::filesystem::read_symlink("/proc/self/fd/3").c_str());
     }
-    auto reporting_threshold = 10zu;
+    auto a_threshold = 10zu;
+    auto p_threshold = 10zu;
     [&](this auto &&self, size_t n, size_t i, size_t pieces, size_t left) {
         if (!left) {
-            if (pieces >= min_pieces) {
-                if (++possible_shapes % reporting_threshold == 0) {
-                    std::print("possible shapes = {} ...\n", possible_shapes);
-                    reporting_threshold *= 10;
-                }
-                if (valid) {
-                    auto sz = shapes.size();
-                    ::write(3, &sz, sizeof(sz));
-                    ::write(3, shapes.data(), shapes.size() * sizeof(Shape<L>));
-                }
+            if (pieces < min_pieces)
+                return;
+            if (++all_shapes % a_threshold == 0) {
+                std::print("all shapes = {} ...\n", all_shapes);
+                a_threshold *= 10;
+            }
+            if (!screening(shapes))
+                return;
+            if (++possible_shapes % p_threshold == 0) {
+                std::print("possible shapes = {} ...\n", possible_shapes);
+                p_threshold *= 10;
+            }
+            if (valid) {
+                auto sz = shapes.size();
+                ::write(3, &sz, sizeof(sz));
+                ::write(3, shapes.data(), shapes.size() * sizeof(Shape<8>));
             }
             return;
         }
@@ -139,14 +147,14 @@ int enumerate_library(size_t area, char *argv[]) {
             n++, i = 0;
         if (n > max_piece_size || left < n || pieces >= max_pieces)
             return;
-        auto sh = shape_at<L>(n, i++);
+        auto sh = shape_at<8>(n, i++);
         self(n, i, pieces, left); // not taking
         shapes.push_back(sh);
         self(n, i, pieces + 1, left - n); // taking
         shapes.pop_back();
     }(min_piece_size, 0, 0, area);
 
-    std::print("possible shapes = {}\n", possible_shapes);
+    std::print("possible shapes = {}/{}\n", possible_shapes, all_shapes);
     return 0;
 }
 
@@ -201,7 +209,11 @@ ddd
     using namespace std::string_view_literals;
 
     if (argv[1] == "enumerate"sv)
-        return enumerate_library(board.base.size() - board.regions.size(), ++argv);
+        return enumerate_library(board.base.size() - board.regions.size(), [&](const auto &shapes) {
+            return screen(shapes
+                | std::views::transform([](Shape<8> sh){ return Piece<L>{ sh.to<L>() }; })
+                | std::ranges::to<std::vector>(), board);
+        }, ++argv);
 
     if (argv[1] == "evaluate"sv)
         return evaluate_libraries(board, ++argv);
