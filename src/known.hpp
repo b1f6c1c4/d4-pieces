d2332833565167ffb9c2bdb0fb4e29bab0902623
b1f6c1c4 <b1f6c1c4@gmail.com>
2025-01-29 12:42:02 -0500

    refactor searchers

--- src/known.hpp
+++ src/known.hpp
@@ -9,7 +9,6 @@ extern const size_t shapes_count[];
 extern const uint64_t * const known_C_shapes[];
 extern const size_t shapes_C_count[];
 
-
 template <size_t L>
 Shape<L> shape_at(size_t n, size_t i) {
     return Shape<8>{ known_shapes[n][i] }.to<L>();
